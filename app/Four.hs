7c2d6ac193357e8b072704500618a5142af0b062
Steve Purcell <steve.purcell@powershop.co.nz>
2016-03-18 10:42:13 +1300

    Golfed version of Four

--- app/Four.hs
+++ app/Four.hs
@@ -1,31 +1,22 @@
 module Four where
 
-import Data.Hash.MD5
-import Data.String.Utils
+import           Data.Hash.MD5
+import           Data.List         (find)
+import           Data.Maybe        (fromJust)
+import           Data.String.Utils
 
 -- "abcdef609043" Hashes to "000001dbbfa"
 
 -- value ++ (show 5)
 
-bruteForce :: String -> Int -> Int
-bruteForce initialValue seed =
-  nextHash initialValue seed (foundMatch hashedString)
-    where
-      hashedString = hashString initialValue seed
-
-nextHash :: String -> Int -> Bool -> Int
-nextHash value seed True = seed
-nextHash value seed False = bruteForce value (seed + 1)
-
-foundMatch :: String -> Bool
-foundMatch hash =
-  startswith "00000" hash
-
 hashString :: String -> Int -> String
-hashString value seed = md5s $ Str (value ++ (show seed))
+hashString value seed = md5s $ Str (value ++ show seed)
+
+bruteForce :: String -> Int
+bruteForce seed = fromJust $ find (startswith "00000" . hashString seed) [0..]
 
 four :: IO Int
 four = do
   initialValue <- readFile "input/4.txt"
-  return $ bruteForce (strip initialValue) 0
+  return $ bruteForce (strip initialValue)
 
