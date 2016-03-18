13344e0dc3c60cba7a8cbb39e7114bdfea717d87
Steve Purcell <steve@sanityinc.com>
2016-03-18 20:58:27 +1300

    Ten: fix hlint warnings, use iterate

--- app/Ten.hs
+++ app/Ten.hs
@@ -1,18 +1,18 @@
 module Ten where
 
 lookAndSay :: String -> String
+lookAndSay [] = []
 lookAndSay (c:[]) = "1" ++ [c]
 lookAndSay (c:cs) = runLength c cs 1
 
 runLength :: Char -> String -> Int -> String
-runLength c [] n = (show n) ++ [c]
+runLength c [] n = show n ++ [c]
 runLength c (x:xs) n
   | c == x    = runLength c xs (n + 1)
-  | otherwise = (runLength c [] n) ++ (lookAndSay (x:xs))
+  | otherwise = runLength c [] n ++ lookAndSay (x:xs)
 
 manyTimes :: Int -> String -> String
-manyTimes 0 str = str
-manyTimes n str = manyTimes (n - 1) (lookAndSay str)
+manyTimes n str = iterate lookAndSay str !! (n - 1)
 
 ten :: IO Int
 ten = do
