ecdf4e210897b2cb97b468ab783580531a83eb08
Steve Purcell <steve@sanityinc.com>
2016-03-18 20:49:57 +1300

    Eight: fix hlint warnings

--- app/Eight.hs
+++ app/Eight.hs
@@ -1,6 +1,6 @@
 module Eight where
 
-decodedLength :: [Char] -> Int
+decodedLength :: String -> Int
 decodedLength [] = 0
 
 decodedLength ('\\':stuff) =
@@ -8,12 +8,13 @@ decodedLength ('\\':stuff) =
   '\\':rest -> 1 + decodedLength rest
   '"':rest -> 1 + decodedLength rest
   'x':rest -> 1 + decodedLength (drop 2 rest)
+  _ -> error "illegal escape"
 decodedLength ['"'] = 0
 decodedLength ('"':xs) = decodedLength xs
-decodedLength (x:xs) = 1 + decodedLength xs
+decodedLength (_:xs) = 1 + decodedLength xs
 
 extraSpace :: String -> Int
-extraSpace encoded = length(encoded) - decodedLength(encoded)
+extraSpace encoded = length encoded - decodedLength encoded
 
 calculateExtraSpace :: [String] -> Int
 calculateExtraSpace input = sum allLines
