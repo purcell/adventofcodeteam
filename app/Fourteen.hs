6fa14d9e5e8c5cc0405c7faa62ad792e051b1a9a
Steve Purcell <steve@sanityinc.com>
2016-03-18 21:13:49 +1300

    Fourteen: apply hlint suggestions

--- app/Fourteen.hs
+++ app/Fourteen.hs
@@ -1,7 +1,8 @@
 module Fourteen where
 
-import Text.Regex
-import Data.Maybe (fromJust)
+import           Data.Maybe (fromJust)
+import           Prelude    hiding (cycle)
+import           Text.Regex
 
 type Reindeer = (Int, Int, Int)
 
@@ -12,12 +13,12 @@ reindeer str = let [a, b, c] = map read numbers
 
 distanceFlown :: Int -> Reindeer -> Int
 distanceFlown seconds (speed, fly, rest) = speed * flightTime
-  where flightTime = full * fly + (min fly partial)
+  where flightTime = full * fly + min fly partial
         (full, partial) = seconds `divMod` cycle
         cycle = fly + rest
 
 fourteen :: IO Int
 fourteen = do
   input <- readFile "input/14.txt"
-  return . maximum $ map (distanceFlown 2503 . reindeer) (lines $ input)
+  return . maximum . map (distanceFlown 2503 . reindeer) . lines $ input
 
