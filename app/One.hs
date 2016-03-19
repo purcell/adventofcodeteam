44963d26b8ab55bdfc3f22feac3f63467dae6698
Steve Purcell <steve@sanityinc.com>
2016-03-19 18:14:51 +1300

    Second part of 1

--- app/One.hs
+++ app/One.hs
@@ -1,6 +1,7 @@
 module One where
 
-
+import           Data.List  (find)
+import           Data.Maybe (fromJust)
 
 calculateFloor :: String -> Int
 calculateFloor = foldl (flip upOrDown) 0
@@ -10,8 +11,12 @@ upOrDown '(' current = current + 1
 upOrDown ')' current = current - 1
 upOrDown _ _ = error "unexpected char"
 
+calculateFirstBasementPosition :: String -> Int
+calculateFirstBasementPosition instr =
+  fst . fromJust . find ((== -1) . snd) $ zip [0..] $ scanl (flip upOrDown) 0 instr
 
-one :: IO Int
+one :: IO (Int, Int)
 one = do
   text <- readFile "input/1.txt"
-  return $ calculateFloor text
+  return ( calculateFloor text
+         , calculateFirstBasementPosition text )
