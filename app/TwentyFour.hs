32f33795f9c45f64f9224bf6857b482caed8e50e
Steve Purcell <steve@sanityinc.com>
2016-03-28 10:18:29 +1300

    Part 2 of 24

--- app/TwentyFour.hs
+++ app/TwentyFour.hs
@@ -3,24 +3,31 @@ module TwentyFour where
 import           Control.Monad (guard)
 import           Data.Function (on)
 import           Data.List     (groupBy, sortBy, tails, unfoldr, (\\))
-import           Data.Maybe    (fromJust, listToMaybe)
 import           Data.Ord      (comparing)
 
 newtype Package = Package { weight :: Int }
                   deriving (Show, Eq, Ord)
 
-grouped3 :: [Package] -> Maybe [[Package]]
-grouped3 pkgs = listToMaybe $ threeGroups limit pkgs
-  where limit = totalWeight pkgs `div` 3
+threeGroups :: [Package] -> [[[Package]]]
+threeGroups pkgs = let limit = totalWeight pkgs `div` 3 in
+  do
+    group1 <- bestGroups limit pkgs
+    group2 <- groupsOfWeight limit (pkgs \\ group1)
+    group3 <- groupsOfWeight limit ((pkgs \\ group1) \\ group2)
+    let groups = [group1, group2, group3] in do
+      guard (length pkgs == sum (map length groups))
+      return groups
 
-threeGroups :: Int -> [Package] -> [[[Package]]]
-threeGroups limit pkgs = do
-  group1 <- bestGroups limit pkgs
-  group2 <- groupsOfWeight limit (pkgs \\ group1)
-  group3 <- groupsOfWeight limit ((pkgs \\ group1) \\ group2)
-  let groups = [group1, group2, group3] in do
-    guard (length pkgs == sum (map length groups))
-    return groups
+fourGroups :: [Package] -> [[[Package]]]
+fourGroups pkgs = let limit = totalWeight pkgs `div` 4 in
+  do
+    group1 <- bestGroups limit pkgs
+    group2 <- groupsOfWeight limit (pkgs \\ group1)
+    group3 <- groupsOfWeight limit ((pkgs \\ group1) \\ group2)
+    group4 <- groupsOfWeight limit (((pkgs \\ group1) \\ group2) \\ group3)
+    let groups = [group1, group2, group3, group4] in do
+      guard (length pkgs == sum (map length groups))
+      return groups
 
 bestGroups :: Int -> [Package] -> [[Package]]
 bestGroups limit pkgs = concatMap (sortBy (comparing entanglement)) $ groupBy ((==) `on` length) $ groupsOfWeight limit pkgs
@@ -54,5 +61,9 @@ packagesFrom file = do
   text <- readFile file
   return $ Package . read <$> lines text
 
-twentyFour :: IO Int
-twentyFour = entanglement . head . fromJust . grouped3 <$> packagesFrom "input/24.txt"
+twentyFour :: IO (Int, Int)
+twentyFour = do
+  pkgs <- packagesFrom "input/24.txt"
+  return (answer threeGroups pkgs,
+          answer fourGroups pkgs)
+    where answer f = entanglement . head . head . f
